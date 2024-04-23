f27e9e1a9273ae8a0bc9f924625ff61c0998bc3f
Ainan <ainanhassan123@gmail.com>
2024-04-23 19:28:05 +0300

    form submit data and display on index page done

--- routes/index.js
+++ routes/index.js
@@ -22,5 +22,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messages Board', messages: messages });
 });
 
+/* GET new message form. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New message', messages: messages });
+});
+
+router.post('/new' , function(req, res, next) {
+    var body = req.body 
+  
+   var newMessages =  messages.push({text: body.message, user: body.author, added: new Date()});
+   console.log(newMessages)
+  res.redirect('/');
+});
+
  
 module.exports = router;
